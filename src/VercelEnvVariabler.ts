c95ad712a91130fd39abc2590216422821573313
Derek Kershner <dkershner@dkershner.com>
2021-05-30 18:57:15 -0700

    Initial commit with env variable setting functionality

---
+++ src/VercelEnvVariabler.ts
@@ -0,0 +1,242 @@
+import axios, { AxiosInstance } from "axios";
+import { info } from "@actions/core";
+
+type VercelEnvVariableType = "encrypted" | "plain";
+
+const VALID_TARGETS = ["encrypted", "plain"];
+
+// eslint-disable-next-line no-shadow
+enum VercelEnvVariableTarget {
+  Production = "production",
+  Preview = "preview",
+  Development = "development",
+}
+
+interface VercelEnvVariable {
+  type: VercelEnvVariableType;
+  value: string;
+  target: VercelEnvVariableTarget[];
+  configurationId?: string;
+  id: string;
+  key: string;
+  createdAt: number;
+  updatedAt: number;
+  createdBy: string;
+  updatedBy: string;
+}
+
+export default class VercelEnvVariabler {
+  private envVariableKeys = new Array<string>();
+  private vercelClient: AxiosInstance;
+
+  private vercelProjectId: string | undefined;
+  private existingEnvVariables: Record<
+    VercelEnvVariableTarget,
+    Record<string, VercelEnvVariable>
+  > = { production: {}, preview: {}, development: {} };
+
+  constructor(
+    private token: string,
+    private projectName: string,
+    envVariableKeysAsString: string,
+    private teamId: string | undefined
+  ) {
+    const envVariableKeys = envVariableKeysAsString?.split(",");
+
+    if (envVariableKeys?.length > 0) {
+      this.envVariableKeys = envVariableKeys;
+    }
+
+    if (!this.token || !this.projectName || this.envVariableKeys.length === 0) {
+      throw new Error("Missing required input(s).");
+    }
+
+    this.vercelClient = axios.create({
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      baseURL: "https://api.vercel.com/v1",
+      params: {
+        teamId: this.teamId,
+      },
+    });
+  }
+
+  public async populateExistingEnvVariables(): Promise<void> {
+    const projectResponse = await this.vercelClient.get<{
+      env: VercelEnvVariable[];
+      id: string;
+    }>(`/projects/${this.projectName}`);
+
+    this.vercelProjectId = projectResponse?.data?.id;
+    if (!this.vercelProjectId) {
+      throw new Error(`Project ${this.projectName} not found`);
+    }
+
+    const env = projectResponse?.data?.env;
+    if (env) {
+      info(`Found ${env.length} existing env variables`);
+
+      for (const existingEnvVariable of env) {
+        for (const existingTarget of existingEnvVariable.target) {
+          const preExistingVariablesForTarget =
+            this.existingEnvVariables[existingTarget] ?? {};
+          this.existingEnvVariables[existingTarget] = {
+            ...preExistingVariablesForTarget,
+            [existingEnvVariable.key]: existingEnvVariable,
+          };
+        }
+      }
+    }
+  }
+
+  public async processEnvVariables(): Promise<void> {
+    for (const envVariableKey of this.envVariableKeys) {
+      await this.processEnvVariable(envVariableKey);
+    }
+  }
+
+  private async processEnvVariable(envVariableKey: string) {
+    const { value, targets, type } = this.parseAndValidateEnvVariable(
+      envVariableKey
+    );
+
+    const existingVariables = targets.reduce((result, target) => {
+      result[target] = this.existingEnvVariables?.[target]?.[envVariableKey];
+      return result;
+    }, {} as Record<VercelEnvVariableTarget, VercelEnvVariable>);
+
+    const existingTargets = Object.keys(existingVariables);
+    if (existingTargets.length === 0) {
+      info(`No existing variable found for ${envVariableKey}, creating.`);
+      await this.createEnvVariable({
+        key: envVariableKey,
+        value,
+        targets,
+        type,
+      });
+    } else if (existingTargets.length !== targets.length) {
+      const newTargets = targets.filter(
+        (target) => !existingTargets.includes(target)
+      );
+      info(
+        `Existing variable found for ${envVariableKey}, but with ${newTargets.join(
+          ","
+        )} as new targets.`
+      );
+      await this.createEnvVariable({
+        key: envVariableKey,
+        value,
+        targets: newTargets,
+        type,
+      });
+      if (type === "plain") {
+        this.processPossibleEnvVariableUpdate({
+          value,
+          existingVariables,
+        });
+      }
+    } else if (type === "plain") {
+      info(
+        `Existing variable found for ${envVariableKey}, comparing plain values.`
+      );
+      this.processPossibleEnvVariableUpdate({
+        value,
+        existingVariables,
+      });
+    } else {
+      info(
+        `Existing variable found for ${envVariableKey}, since it is encrypted, assuming it is equal.`
+      );
+    }
+  }
+
+  private parseAndValidateEnvVariable(
+    envVariableKey: string
+  ): {
+    value: string;
+    targets: VercelEnvVariableTarget[];
+    type: VercelEnvVariableType;
+  } {
+    const value = process.env[envVariableKey];
+
+    const targetString = process.env[`TARGET_${envVariableKey}`];
+    const type = process.env[`TYPE_${envVariableKey}`] as VercelEnvVariableType;
+
+    if (!value) {
+      throw new Error(
+        `Variable ${envVariableKey} is missing env variable: ${envVariableKey}`
+      );
+    }
+    if (!targetString) {
+      throw new Error(
+        `Variable ${envVariableKey} is missing env variable: ${`TARGET_${envVariableKey}`}`
+      );
+    }
+    if (!type) {
+      throw new Error(
+        `Variable ${envVariableKey} is missing env variable: ${`TYPE_${envVariableKey}`}`
+      );
+    }
+
+    const targets = targetString
+      .split(",")
+      .filter((target) =>
+        VALID_TARGETS.includes(target)
+      ) as VercelEnvVariableTarget[];
+
+    if (targets.length === 0) {
+      throw new Error(
+        `No valid targets found for ${envVariableKey}, targets given: ${targetString}, valid targets: ${VALID_TARGETS.join(
+          ","
+        )}`
+      );
+    }
+
+    return { value, targets, type };
+  }
+
+  private async createEnvVariable({
+    type,
+    key,
+    value,
+    targets,
+  }: {
+    key: string;
+    value: string;
+    targets: VercelEnvVariableTarget[];
+    type: VercelEnvVariableType;
+  }) {
+    const createResponse = await this.vercelClient.post(
+      `/projects/${this.projectName}/env`,
+      { type, key, value, target: targets }
+    );
+
+    if (!createResponse?.data) {
+      info(
+        `Variable ${key} with targets ${targets.join(",")} created successfully`
+      );
+    }
+  }
+
+  private async processPossibleEnvVariableUpdate({
+    value,
+    existingVariables,
+  }: {
+    value: string;
+    existingVariables: Record<VercelEnvVariableTarget, VercelEnvVariable>;
+  }) {
+    const existingVariable = Object.values(existingVariables)[0]; // They are all actually the same
+    if (existingVariable.type === "plain" && existingVariable.value !== value) {
+      info(
+        `Value for env variable ${existingVariable.key} has found to have changed, updating value`
+      );
+      await this.vercelClient.patch(
+        `/projects/${this.projectName}/env/${existingVariable.id}`,
+        { value }
+      );
+    } else {
+      info(`No change found for ${existingVariable.key}, skipping...`);
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { getInput, setFailed, info } from "@actions/core";
+import VercelEnvVariabler from "./VercelEnvVariabler";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token: string = getInput("token", { required: true });
+    const projectName: string = getInput("projectName", { required: true });
+    const envVariableKeys: string = getInput("envVariableKeys", {
+      required: true,
+    });
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const teamId: string = getInput("teamId");
 
-    core.setOutput('time', new Date().toTimeString())
+    const envVariabler = new VercelEnvVariabler(
+      token,
+      projectName,
+      envVariableKeys,
+      teamId
+    );
+
+    await envVariabler.populateExistingEnvVariables();
+    await envVariabler.processEnvVariables();
+    info("Vercel env variables completed successfully");
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message);
   }
 }
 
-run()
+run();
